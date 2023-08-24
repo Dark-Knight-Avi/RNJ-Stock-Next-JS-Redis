d4453338153353e99aafedd91af0101d978b8dba
Aritra Karmakar <64675840+Dark-Knight-Avi@users.noreply.github.com>
2023-08-24 20:59:51 +0530

    fetchData Removed

--- app/api/getAllCategories/route.js
+++ app/api/getAllCategories/route.js
@@ -1,20 +1,13 @@
 import { getAllCategoryKeys } from "@/app/helper/getAllCategoryKeys";
 import { db } from "@/app/lib/db";
 import { NextResponse } from "next/server";
-const fetchData = async (command, ...args) => {
-  try {
-    return await db[command](...args);
-  } catch (error) {
-    console.error("Error executing Redis command:", error);
-    throw error;
-  }
-}
+
 export async function GET(request) {
   const allCategoryKeys = await getAllCategoryKeys()
   const allCategories = []
 
   for(const key of allCategoryKeys) {
-    const categoryValue = await fetchData('get', key)
+    const categoryValue = await db.get(key)
     allCategories.push(categoryValue)
   }
 
