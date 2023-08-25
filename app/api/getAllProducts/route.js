fb6b73de8b267c2da17719f42baa618ece7f01aa
Aritra Karmakar <64675840+Dark-Knight-Avi@users.noreply.github.com>
2023-08-25 22:14:05 +0530

    fix: again getAppProducts

--- app/api/getAllProducts/route.js
+++ app/api/getAllProducts/route.js
@@ -4,20 +4,22 @@ import { NextResponse } from "next/server";
 
 export async function GET(request) {
   try {
-    const cursor = '0'; // Initial cursor
     const pattern = 'pdt:*'; // Pattern to match keys
-
-    const scanResult = await fetchfromredis('scan', cursor, 'MATCH', pattern);
-    console.log(scanResult)
-    const matchingKeys = scanResult[1];
+    let cursor = '0'; // Initial cursor
 
     const products = [];
 
-    // Fetch values for each matching key using the 'matchingKeys' array
-    for (const key of matchingKeys) {
-      const productValue = await fetchredis('get', key);
-      products.push(productValue); // Assuming 'productValue' is a JSON-parsed string
-    }
+    do {
+      const scanResult = await fetchfromredis('scan', cursor, 'MATCH', pattern);
+      cursor = scanResult[0];
+      const matchingKeys = scanResult[1];
+
+      // Fetch values for each matching key using the 'matchingKeys' array
+      for (const key of matchingKeys) {
+        const productValue = await fetchredis('get', key);
+        products.push(productValue); // Assuming 'productValue' is a JSON-parsed string
+      }
+    } while (cursor !== '0'); // Continue until cursor becomes '0'
 
     return NextResponse.json({ type: 'All Products', products });
   } catch (error) {
