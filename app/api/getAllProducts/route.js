bbd2cef04dbe81dcb59c9570da434e04e0824dda
Aritra Karmakar <64675840+Dark-Knight-Avi@users.noreply.github.com>
2023-08-26 11:09:20 +0530

    fix: page based empty stock added

--- app/api/getAllProducts/route.js
+++ app/api/getAllProducts/route.js
@@ -2,12 +2,15 @@ import { fetchredis } from "@/app/helper/redis";
 import { fetchfromredis } from "@/app/helper/rediscpy";
 import { NextResponse } from "next/server";
 
-export async function GET(request) {
+export async function POST(request) {
   try {
+    const { page } = await request.json(); // Extract 'page' from the request body
+    const productsPerPage = 10;
     const pattern = 'pdt:*'; // Pattern to match keys
     let cursor = '0'; // Initial cursor
 
     const products = [];
+    let productsCount = 0;
 
     do {
       const scanResult = await fetchfromredis('scan', cursor, 'MATCH', pattern);
@@ -16,12 +19,15 @@ export async function GET(request) {
 
       // Fetch values for each matching key using the 'matchingKeys' array
       for (const key of matchingKeys) {
-        const productValue = await fetchredis('get', key);
-        products.push(productValue); // Assuming 'productValue' is a JSON-parsed string
+        if (productsCount >= (page - 1) * productsPerPage && productsCount < page * productsPerPage) {
+          const productValue = await fetchredis('get', key);
+          products.push(productValue); // Assuming 'productValue' is a JSON-parsed string
+        }
+        productsCount++;
       }
-    } while (cursor !== '0'); // Continue until cursor becomes '0'
+    } while (cursor !== '0' && productsCount < page * productsPerPage); // Continue until cursor becomes '0' or reached desired page products
 
-    return NextResponse.json({ type: 'All Products', products });
+    return NextResponse.json({ type: 'Page Products', page, products });
   } catch (error) {
     console.error(error);
     return NextResponse.json({ error: 'An error occurred while fetching products.' });
--- app/page.js
+++ app/page.js
@@ -9,14 +9,29 @@ import ProductCard from "./components/ProductCard";
 export default function Home() {
   const [products, setProducts] = useState(null)
   const [error, setError] = useState(null); // Add state to handle errors
-
+  const [page, setPage] = useState(1)
+  const nextPage = () => {
+    setProducts(null)
+    if (products.length !== 0) {
+      setPage(page + 1)
+    }
+  }
+  const previousPage = () => {
+    setProducts(null)
+    setPage(page === 1 ? 1 : page - 1)
+  }
+  const goToPage1 = () => {
+    setProducts(null)
+    setPage(1)
+  }
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await axios.get('/api/getAllProducts', {
-          timeout: 30000
+        const response = await axios.post('/api/getAllProducts', {
+          page
         });
         setProducts(response.data.products);
+
       } catch (error) {
         console.error('Error fetching products:', error);
         setError('An error occurred while fetching products.');
@@ -24,10 +39,15 @@ export default function Home() {
     };
 
     fetchData();
-  }, []);
+  }, [page]);
 
   if (error) {
-    return <div>Error: {error}</div>;
+    return <React.Fragment>
+      Error: {error}
+      <section className="flex w-full justify-end items-center mt-3 px-3">
+        <button onClick={previousPage} className="px-3 text-center py-2 border hover:bg-slate-700 active:bg-slate-800 mr-2">Previous Page</button>
+      </section>
+    </React.Fragment>;
   }
 
   if (!products) {
@@ -38,15 +58,20 @@ export default function Home() {
   return (
     <React.Fragment>
       <section className="mx-3 border border-white mt-5 min-h-[80px] flex justify-between items-center flex-col">
-        <div className="text-2xl font-bold m-3">Empty Stocks</div>
+        <div className="text-2xl font-bold m-3">Empty Stocks - Page {page}</div>
         <div className="emptyProducts flex flex-col justify-start items-center w-full h-full border-t border-white">
           <div className='w-full border border-white h-[40px] flex justify-between items-center px-1'>
             <div className="text-sm text-center font-bold w-[33%]">Product Name</div>
             <div className="text-sm text-center font-bold w-[33%]">Category</div>
             <div className="text-sm text-center font-bold w-[33%]">Size/Weight</div>
           </div>
-          {products.filter((product) => product.quantity === 0).map((product) => <ProductCard key={product.productId} productId={product.productId} productName={product.productName} category={product.category} subcategory={product.subCategory} size={product.size} weight={product.weight}/>)}
+          {products.filter((product) => product.quantity === 0).map((product) => <ProductCard key={product.productId} productId={product.productId} productName={product.productName} category={product.category} subcategory={product.subCategory} size={product.size} weight={product.weight} />)}
         </div>
+      </section>
+      <section className="flex w-full justify-end items-center mt-3 px-3">
+        <button onClick={goToPage1} className="px-3 text-center py-2 border hover:bg-slate-700 active:bg-slate-800 mr-2">Go to Page 1</button>
+        <button onClick={previousPage} className="px-3 text-center py-2 border hover:bg-slate-700 active:bg-slate-800 mr-2">Previous Page</button>
+        <button onClick={nextPage} className="px-3 text-center py-2 border hover:bg-slate-700 active:bg-slate-800">Next Page</button>
       </section>
     </React.Fragment>
   )
