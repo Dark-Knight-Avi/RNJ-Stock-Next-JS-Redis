64af6178d06cfdcfbaa62e81f9035c421d8ed05c
Aritra Karmakar <64675840+Dark-Knight-Avi@users.noreply.github.com>
2023-09-01 20:21:34 +0530

    Update ProductCard.jsx spelling mistake

--- app/components/ProductCard.jsx
+++ app/components/ProductCard.jsx
@@ -11,7 +11,7 @@ const ProductCard = ({
   return (
     <div className="w-full border border-white h-[40px] flex justify-between items-center px-1">
       <div className="text-xs text-center font-semibold w-[33%]">{productName}</div>
-      <div className="text-xs text-center font-semibold w-[33%]">{subCategory}</div>
+      <div className="text-xs text-center font-semibold w-[33%]">{subcategory}</div>
       <div className="text-xs text-center font-semibold w-[33%]">{size === 'NA' ? weight : size}</div>
     </div>
   );
